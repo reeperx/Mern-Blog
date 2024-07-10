8702a9faaaa2590ed12f6035b80f07a1e1da66af
togb-reeper <togbbots@gmail.com>
2024-07-10 13:37:07 +0200

    updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { Button } from "flowbite-react";
+import { BsInfoSquare, BsNewspaper, BsPciCard, BsTv } from "react-icons/bs";
+import { HiPhotograph } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 export default function Home() {
@@ -7,13 +9,14 @@ export default function Home() {
       <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
         <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
           <div className="flex justify-center">
-            <a
-              className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200"
+            <p></p>
+            <p
+              className="inline-flex text-center font-semibold tex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200"
               href="/about"
             >
-              You know about me, but you don&apos;t know my story!
+              If ye abide in me, and my words abide in you, <br />ye shall ask what ye will, and it shall be done unto you
               <span></span>
-            </a>
+            </p>
           </div>
 
           <div className="mt-5 max-w-2xl text-center mx-auto">
@@ -46,24 +49,9 @@ export default function Home() {
             className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
             href="#"
           >
-            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <rect width="10" height="14" x="3" y="8" rx="2" />
-                <path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4" />
-                <path d="M8 18h.01" />
-              </svg>
-            </div>
+            
+              <BsNewspaper className="h-10 w-10" />
+            
             <div className="mt-5">
               <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
                 Articles
@@ -78,25 +66,7 @@ export default function Home() {
             className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
             href="#"
           >
-            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M20 7h-9" />
-                <path d="M14 17H5" />
-                <circle cx="17" cy="17" r="3" />
-                <circle cx="7" cy="7" r="3" />
-              </svg>
-            </div>
+            <BsTv className="h-10 w-10" />
             <div className="mt-5">
               <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
                 Videos
@@ -111,23 +81,7 @@ export default function Home() {
             className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
             href="#"
           >
-            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-              </svg>
-            </div>
+            <HiPhotograph className="h-10 w-10" />
             <div className="mt-5">
               <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
                 Pictures
@@ -142,23 +96,7 @@ export default function Home() {
             className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
             href="#"
           >
-            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
-                <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
-              </svg>
-            </div>
+            <BsInfoSquare className="h-10 w-10" />
             <div className="mt-5">
               <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
                 24/7 Content
