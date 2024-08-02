a54eaa9f5f7a25ef4a55c7b0e2ce873af8cc425c
togb-reeper <togbbots@gmail.com>
2024-08-02 11:42:29 +0200

    chnages

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -17,17 +17,16 @@ export default function Blogs() {
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto h-screen">
-        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <h1 className="text-3xl font-bold lg:text-6xl">News and Articles</h1>
         <p className="text-gray-500 text-xs sm:text-sm">
           Here you&apos;ll find a variety of articles detailing my story and
           learn the truth behind it all.
         </p>
-        <Link
-          to="/search"
-          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
-        >
-          View all posts
-        </Link>
+        <div className="flex flex-wrap gap-4">
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
       </div>
       {/** <div className="p-3 bg-amber-100 dark:bg-slate-700"> 
         <CallToAction />
@@ -36,17 +35,11 @@ export default function Blogs() {
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
           <div className="flex flex-col gap-6">
-            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
-              {posts.map((post) => (
-                <PostCard key={post._id} post={post} />
-              ))}
-            </div>
             <Link
               to={"/search"}
               className="text-lg text-teal-500 hover:underline text-center"
             >
-              View all posts
+              view all posts
             </Link>
           </div>
         )}
